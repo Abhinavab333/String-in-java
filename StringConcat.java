c59f6fd9d6d51478144d34187772972d24fce015
avabhi <32228794+avabhi@users.noreply.github.com>
2018-04-04 00:54:41 +0530

    another topic

---
+++ StringConcat.java
@@ -0,0 +1,12 @@
+class StringConcat
+{
+	public static void main(String args[])
+	{
+		String s1="abhinav"+"vishwakarma";
+		System.out.println(s1);
+		String s2="Rkgit";
+		System.out.println(s1.concat(s2));
+		//Substring in string
+		System.out.println(s1.substring(1,3));
+	}
+}
